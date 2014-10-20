bcc738e76ee9fc42c715b525ccb192f6d3106125
Edilson Gonzalez <edilson.gonza@unitec.edu>
2014-10-20 16:08:43 -0600

    Campus Scaffold

---
+++ SistemaIntegralIngresos/Models/Campus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaIntegralIngresos.Models
+{
+    public class Campus
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Details { get; set; }
+    }
+}
---
+++ SistemaIntegralIngresos/Models/SIIDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SistemaIntegralIngresos.Models
+{
+    public class SIIDbContext : DbContext
+    {
+        public DbSet<Campus> Campus { get; set; }
+    }
+}
