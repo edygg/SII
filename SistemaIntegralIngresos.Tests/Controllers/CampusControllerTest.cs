edb073de66d8b93bce7994e3e5a9768d792487c3
Wilfredo Mejia <itha92@gmail.com>
2014-10-26 09:41:41 -0600

    trabajando en el unit testing de campus controller

---
+++ SistemaIntegralIngresos.Tests/Controllers/CampusControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SistemaIntegralIngresos.Controllers;
+
+namespace SistemaIntegralIngresos.Tests.Controllers
+{
+    [TestClass]
+    public class CampusControllerTest
+    {
+        [TestMethod]
+        public void TestIndexView()
+        {
+            var controller = new CampusController();
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
--- SistemaIntegralIngresos/Controllers/CampusController.cs
+++ SistemaIntegralIngresos/Controllers/CampusController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SistemaIntegralIngresos.Models;
+using Microsoft.VisualStudio.QualityTools.UnitTestFramework;
 
 namespace SistemaIntegralIngresos.Controllers
 {
