2ce8d1fc474b2245e287eaac41156fdef8828265
Henry23 <gabriel_23hgap@unitec.edu>
2014-10-26 09:38:22 -0600

    unitTesing_CampusController

---
+++ SistemaIntegralIngresos.Tests/Controllers/CampusControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SistemaIntegralIngresos.Tests.Controllers
+{
+    [TestClass]
+    public class CampusControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
