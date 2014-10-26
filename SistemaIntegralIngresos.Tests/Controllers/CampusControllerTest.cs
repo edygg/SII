2cafa3953a16888b76d91c1ba117e71dba3ea1a4
Edilson Gonzalez <edilson.gonza@unitec.edu>
2014-10-26 11:04:05 -0600

    Campus model unit testing

---
+++ SistemaIntegralIngresos.Tests/Controllers/CampusControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SistemaIntegralIngresos.Models;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace SistemaIntegralIngresos.Tests.Controllers
+{
+    [TestClass]
+    public class CampusControllerTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(System.Data.Entity.Validation.DbEntityValidationException))]
+        public void TestCampusWithEmptyName()
+        {
+            var campus = new Campus { Id = 1, Name = "", Code = "UTGU", Details = "" };
+            SIIDbContext context = new SIIDbContext();
+            context.Campus.Add(campus);
+            context.SaveChanges();
+        }
+    }
+}
