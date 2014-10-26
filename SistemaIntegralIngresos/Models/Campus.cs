5b36b4270f736b5826a98aea3e8f40e8fa6efabe
Edilson Gonzalez <edilson.gonza@unitec.edu>
2014-10-25 18:47:22 -0600

    Create users view frontend finished

--- SistemaIntegralIngresos/Models/Campus.cs
+++ SistemaIntegralIngresos/Models/Campus.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,9 @@ namespace SistemaIntegralIngresos.Models
     public class Campus
     {
         public int Id { get; set; }
+        [Required]
         public String Code { get; set; }
+        [Required]
         public String Name { get; set; }
         public String Details { get; set; }
     }
