5ff5d4915867267ed21753be38bf5801ee044f0e
Henry23 <gabriel_23hgap@unitec.edu>
2014-10-27 11:13:46 -0600

    Starting Visit Controller Testing

---
+++ SistemaIntegralIngresos/Models/IVisit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaIntegralIngresos.Models
+{
+    interface IVisit
+    {
+        List<Visit> GetAllCampus();
+        Boolean InsertVisit(Visit campus);
+        Boolean DeleteVisit(int id);
+        Visit FindByID(int id);
+        Boolean EditVisit(Visit campus);
+    }
+}
---
+++ SistemaIntegralIngresos/Models/VisitOperationRepo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaIntegralIngresos.Models
+{
+    public class VisitOperationRepo : IVisit
+    {
+        public List<Visit> GetAllCampus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InsertVisit(Visit campus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteVisit(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Visit FindByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditVisit(Visit campus)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
