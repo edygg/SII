d2d544a08ed2e090c0ab02ccc81a1290abc0e82f
Wilfredo Mejia <itha92@gmail.com>
2014-10-26 18:25:01 -0600

    working in RVA Controller and view

---
+++ SistemaIntegralIngresos/Controllers/RVAController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SistemaIntegralIngresos.Models;
+
+namespace SistemaIntegralIngresos.Controllers
+{
+    public class RVAController : Controller
+    {
+        private SIIDbContext db = new SIIDbContext();
+        //
+        // GET: /RVA/
+
+        public ActionResult Index()
+        {
+            ViewBag.Campus = new SelectList(db.Campus, "Id", "Name");
+
+            return View();
+        }
+
+    }
+}
