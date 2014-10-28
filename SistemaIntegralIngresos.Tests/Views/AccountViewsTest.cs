2325187a60179df6cd40ae990194b514e542857c
Ariel Esquivel <ariel-esquivel@hotmail.com>
2014-10-27 21:59:39 -0700

    Some Views Test

---
+++ SistemaIntegralIngresos.Tests/Views/AccountViewsTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApprovalTests.Asp.Mvc;
+using SistemaIntegralIngresos.Controllers;
+using ApprovalTests.Reporters;
+
+namespace SistemaIntegralIngresos.Tests.Views
+{
+    [TestClass]
+    [UseReporter(typeof(FileLauncherReporter))]
+    public class AccountViewsTest : MvcTest
+    {
+        [TestMethod]
+        public void RegisterAccountViewTest()
+        {
+            MvcApprovals.VerifyMvcPage(new AccountController().Register);
+        }
+
+    }
+}
